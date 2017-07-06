228e2095ac71224f72d96a801d13c2a21d28eac5
erfg12 <newgmobile@gmail.com>
2017-07-06 10:30:51 -0400

    updated codesign script

--- Memory/codesign.bat
+++ Memory/codesign.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p id="Enter private key file pass: "
-"C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "%UserProfile%\Documents\My Private Key.pfx" /p %id% "bin\Debug\Memory.dll"
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x86\signtool" sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "%UserProfile%\Documents\My Private Key.pfx" /p %id% "bin\Debug\Memory.dll"
 pause
