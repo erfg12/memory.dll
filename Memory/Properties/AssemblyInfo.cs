dd290dbd94010c5bd9460cb2c052ab5476a6989f
erfg12 <newgmobile@gmail.com>
2017-08-07 14:50:46 -0400

    version change

--- Memory/Properties/AssemblyInfo.cs
+++ Memory/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Process Memory Read/Write")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Game hacking library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NewAgeSoldier.com")]
+[assembly: AssemblyCompany("New Age Software, LLC")]
+[assembly: AssemblyProduct("newage.software")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.8.0")]
-[assembly: AssemblyFileVersion("1.1.8.0")]
+[assembly: AssemblyVersion("1.1.9.0")]
+[assembly: AssemblyFileVersion("1.1.9.0")]
