b477f4a88869f65bd30569d01c9a532b64de93d2
Jacob Fliss <newgmobile@gmail.com>
2020-10-02 11:26:17 -0400

    fixed XML warnings, fixed compiling error for C# 7.1

--- Memory/memory.cs
+++ Memory/memory.cs
@@ -331,7 +331,7 @@ namespace Memory
         /// <summary>
         /// Open the PC game process with all security and access rights.
         /// </summary>
-        /// <param name="proc">Use process name or process ID here.</param>
+        /// <param name="pid">Use process name or process ID here.</param>
         /// <returns></returns>
         public bool OpenProcess(int pid)
         {
@@ -1471,7 +1471,7 @@ namespace Memory
         /// <param name="code">Address to create the trampoline</param>
         /// <param name="newBytes">The opcodes to write in the code cave</param>
         /// <param name="replaceCount">The number of bytes being replaced</param>
-        /// <param name="allocationSize">size of the allocated region</param>
+        /// <param name="size">size of the allocated region</param>
         /// <param name="file">ini file to look in</param>
         /// <remarks>Please ensure that you use the proper replaceCount
         /// if you replace halfway in an instruction you may cause bad things</remarks>
